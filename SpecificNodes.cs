52e3ca12fc42da6aba698bba377b6be8ed1afcfc
Andres de Lago <delagoandres@gmail.com>
2019-10-14 22:39:16 -0500

    Cleanup of unused Node types on SpecificNodes.cs

--- SpecificNodes.cs
+++ SpecificNodes.cs
@@ -15,40 +15,29 @@ namespace Chimera
     class OrNode : Node { }
     class XorNode : Node { }
     class NotNode : Node { }
-
     class EqualNode : Node { }
     class UnequalNode : Node { }
     class LessThanNode : Node { }
     class MoreThanNode : Node { }
     class LessThanEqualNode : Node { }
     class MoreThanEqualNode : Node { }
-
     class MinusNode : Node { }
     class PlusNode : Node { }
     class TimesNode : Node { }
     class DivNode : Node { }
     class RemNode : Node { }
-
     class ExitNode : Node { }
-
     class IntegerNode : Node { }
     class StringNode : Node { }
     class BooleanNode : Node { }
     class ListTypeNode : Node { }
-
-    class TypeNode : Node { }
-
     class IntLiteralNode : Node { }
     class StringLiteralNode : Node { }
     class BoolLiteralNode : Node { }
     class ListLiteralNode : Node { }
-
     class ConstantDeclarationNode : Node { }
     class ConstantListNode : Node { }
     class IdentifierNode : Node { }
-    class SumExpressionNode : Node { }
-    class RelationalExpressionNode : Node { }
-    class LogicalExpressionNode : Node { }
     class ReturnStatementNode : Node { }
     class LoopStatementNode : Node { }
     class VariableDeclarationNode : Node { }
@@ -61,7 +50,6 @@ namespace Chimera
     class ProcedureDeclarationNode : Node { }
     class ProcedureListNode : Node { }
     class ParameterDeclarationNode : Node { }
-    class SimpleExpressionNode : Node { }
     class AssignmentNode : Node { }
     class CallStatementNode : Node { }
     class CallNode : Node { }
