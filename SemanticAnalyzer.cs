347669e2e779209ca3a4ddf6656ef8d4224eaadc
Servio Tulio Reyes Castillo <A01371719@itesm.mx>
2019-11-29 11:11:48 -0600

    Update dates

--- SemanticAnalyzer.cs
+++ SemanticAnalyzer.cs
@@ -1,11 +1,12 @@
 /*
 Chimera
-Date: 11-Nov-2019
+Date: 2-Dic-2019
 Authors:
 	A01371779 Andres De Lago Gomez
 	A01377503 Ian Neumann Sanchez
 	A01371719 Servio Tulio Reyes Castillo
 */
+
 using System;
 using System.Linq;
 using System.Collections.Generic;
--- SymbolTable.cs
+++ SymbolTable.cs
@@ -1,6 +1,6 @@
 /*
 Chimera
-Date: 11-Nov-2019
+Date: 2-Dic-2019
 Authors:
 	A01371779 Andres De Lago Gomez
 	A01377503 Ian Neumann Sanchez
